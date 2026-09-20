33b24753fae549e67a863c9bf7fe11a7cd267671
dharmendra <dtechnicaldk@gmail.com>
2026-09-20 14:59:09 +0530

    fea/firstFeature -from feature branch

---
+++ script.js
@@ -0,0 +1,8 @@
+//main: simple initial log
+console.log("welcome to initial console");
+
+//feature 1: from ffeature branch added
+const firstFeature = () => {
+  console.log("i am the first feature added from feature branch \n");
+};
+firstFeature();
